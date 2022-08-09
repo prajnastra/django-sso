2a9afd7111f8d8170dbb5ee34a805eb5fb1861c7
ethabhijit <coder.abhijit90@gmail.com>
2022-08-09 15:07:07 +0530

    Initial commit after forking

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fraudify_sso.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sso_server.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- sso_server/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SsoServerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sso_server'
